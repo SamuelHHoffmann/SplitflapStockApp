ec35bdd79a0f682338f6cbd68bda5bdeca37d412
samuelhhoffmann <samuel.hoffmann@stonybrook.edu>
2021-05-07 20:43:39 -0400

    Initial Commit

---
+++ splitflap-stockapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  splitflap-stockapp
+//
+//  Created by Samuel Hoffmann on 5/7/21.
+//  Copyright © 2021 Samuel Hoffmann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
