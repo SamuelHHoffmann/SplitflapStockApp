c9633afe298eedf1828a1542b36dceebe0a319c0
samuelhhoffmann <samuelhoffmann.development@gmail.com>
2021-05-31 17:11:35 -0400

    format change

--- splitflap-stockapp/ViewController.swift
+++ splitflap-stockapp/ViewController.swift
@@ -81,8 +81,6 @@ class ViewController: UIViewController {
             buttonTemp.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
             splitDisplayButtons.append(buttonTemp)
             self.bodyView.addSubview(buttonTemp)
-            
-            
         }
         
         
